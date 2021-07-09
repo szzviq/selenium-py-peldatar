5dbda1411cf29e803bd35e2dab32ab36f97dd06a
SzaboZichy Viktoria <vszabo5@gmail.com>
2021-07-09 21:24:40 +0200

    filekezeles

--- python2-homework/fajl4.py
+++ python2-homework/fajl4.py
@@ -1,8 +1,6 @@
 #Olvasd be a fájlt, tárold a sorokat listában, majd írd ki a lista tartalmát így, ahogy beolvastad, soronként egy szóval egy másik fájlba!
 with open('adat.txt', 'r') as mese:
-        # res.append(line.split(None,1)[0])
-        mese_list = mese.readlines()
+         mese_list = mese.readlines()
 
 with open ('adat3.txt','w') as res:
     res.writelines(mese_list)
-    # (" ".join(mese_list))
